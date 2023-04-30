1198d7175c6b40d3774580516f4ffd03fc9b4819
Marco Siccardi <68176309+MSicc@users.noreply.github.com>
2023-04-30 07:51:28 +0200

    Update AppDelegateEx.cs

--- src/IosOrientationLockMAUI/Platforms/iOS/AppDelegateEx.cs
+++ src/IosOrientationLockMAUI/Platforms/iOS/AppDelegateEx.cs
@@ -9,8 +9,8 @@ namespace IosOrientationLockMAUI.Platforms.iOS
     {
         public virtual UIInterfaceOrientationMask CurrentLockedOrientation { get; set; }
 
-        //according to the Apple docs, this gets called on every orientation change, so this one is the second essential step!
-        //https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1623107-application
+        //according to the Apple docs, Application and ViewController have to agree on the supported orientation, this forces it
+        //https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1623107-application?language=objc
         [Foundation.Export("application:supportedInterfaceOrientationsForWindow:")]
         public virtual UIInterfaceOrientationMask GetSupportedInterfaceOrientationsForWindow(UIApplication application, UIWindow forWindow)
             => this.CurrentLockedOrientation;
